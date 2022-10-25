fe890ceb744850b9f6abb2e982c21c75c8179409
Bojan Šernek <bojan@distopiksound.com>
2022-10-25 23:57:20 +0800

    non-windows safe paths removed from repo

--- domain/audiocloud-domain-server/src/db/migrations.rs
+++ domain/audiocloud-domain-server/src/db/migrations.rs
@@ -7,7 +7,7 @@ use crate::db::{DataOpts, Db};
 #[instrument(skip_all, err)]
 pub async fn execute(batis: &mut Rbatis) -> anyhow::Result<usize> {
     let user_version: usize = batis.fetch_decode("PRAGMA user_version", vec![]).await?;
-    let migrations = vec![include_str!("migrations/2022-10-25T10:45:54.093Z_init.sql")];
+    let migrations = vec![include_str!("migrations/2022-10-25T1045Z_init.sql")];
 
     let mut count = 0;
 
