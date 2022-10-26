15398adb5249f01a4fe83c1cf18fd9d2570fb204
Bojan Šernek <bojan@distopiksound.com>
2022-10-26 18:46:13 +0800

    better startup error message on config file missing, attempt a different fix for builds.

--- domain/audiocloud-domain-server/src/config/file.rs
+++ domain/audiocloud-domain-server/src/config/file.rs
@@ -1,9 +1,14 @@
 use std::path::PathBuf;
+
+use anyhow::anyhow;
 use tracing::*;
 
 use audiocloud_api::cloud::domains::DomainConfig;
 
 #[instrument(skip_all, err)]
 pub async fn get_config(path: PathBuf) -> anyhow::Result<DomainConfig> {
-    Ok(serde_yaml::from_slice(std::fs::read(path)?.as_slice())?)
+    Ok(serde_yaml::from_slice(std::fs::read(&path).map_err(|err| {
+                                                      anyhow!("Could not open config file: {path:?}: {err}")
+                                                  })?
+                                                  .as_slice())?)
 }
