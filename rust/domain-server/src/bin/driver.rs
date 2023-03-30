adf1af1d596c7c78a082950e5c2b6473475c9aeb
baadc0de <bojan@distopik.com>
2023-03-30 17:57:25 +0300

    change logging

--- rust/domain-server/src/bin/driver.rs
+++ rust/domain-server/src/bin/driver.rs
@@ -29,7 +29,19 @@ struct ServerState {
 
 #[tokio::main]
 async fn main() {
-  tracing_subscriber::fmt::init();
+  let subscriber = tracing_subscriber::fmt()
+    // Use a more compact, abbreviated log format
+    .compact()
+    // Display source code file paths
+    .with_file(true)
+    // Display source code line numbers
+    .with_line_number(true)
+    // Display the thread ID an event was recorded on
+    .with_thread_ids(true)
+    // Don't display the event's target (module path)
+    .with_target(false)
+    // Build the subscriber
+    .finish();
 
   let (tx_cmd, rx_cmd) = mpsc::channel(0xff);
   let (tx_evt, mut rx_evt) = mpsc::channel(0xff);
