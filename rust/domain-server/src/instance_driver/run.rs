fe9cb2814f27ada965de91b6cb082ad8e969c297
baadc0de <bojan@distopik.com>
2023-03-30 17:52:30 +0300

    log events as TRACE

--- rust/domain-server/src/instance_driver/run.rs
+++ rust/domain-server/src/instance_driver/run.rs
@@ -60,7 +60,7 @@ pub async fn run_driver_server<Drv: Driver>(instance_id: String,
             }
             | Ok(events) => {
               for event in events {
-                info!(instance_id, ?event, "generated event");
+                trace!(instance_id, ?event, "generated event");
                 tx_evt.send(event).await?;
               }
             }
