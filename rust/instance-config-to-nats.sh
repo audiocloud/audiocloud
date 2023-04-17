adc91dbad109e8b30d46ea0be3742fe4bde5d79a
Rok Kroflič <rok.kroflic@gmail.com>
2023-04-17 14:58:35 +0200

    Add kv-reset and instance-config-to-nats bash scripts.

---
+++ rust/instance-config-to-nats.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+export NATS_URL=10.1.0.10:4222
+
+./target/debug/ac --nats-url=${NATS_URL} instance put btrmkr_ml_1 config/instances/btrmkr_ml_1.yaml --host raspineve --mocked
