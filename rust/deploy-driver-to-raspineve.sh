c5b06639221c0b80e6b689fad0813a5899d0227c
Rok Kroflič <rok.kroflic@gmail.com>
2023-05-11 14:26:54 +0200

    Add start-domain-server.sh.

--- rust/deploy-driver-to-raspineve.sh
+++ rust/deploy-driver-to-raspineve.sh
@@ -18,5 +18,5 @@ export PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig
 export PKG_CONFIG_SYSROOT_DIR=/usr/arm-linux-gnueabihf/sysroot
 
 cargo build --release --target=${TARGET_ARCH} -p domain-server --bin domain_server
-rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
-ssh -t ${TARGET_HOST} ${TARGET_PATH}
+# rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
+# ssh -t ${TARGET_HOST} ${TARGET_PATH}
--- rust/instance-config-to-nats.sh
+++ rust/instance-config-to-nats.sh
@@ -9,5 +9,5 @@ export NATS_URL=${NATS_URL:-10.1.0.10:4222}
 
 ./target/debug/ac --nats-url=${NATS_URL} instance put ppdu_r1_up_l config/instances/ppdu_r1_up_l.yaml --host raspineve --mocked
 
-./target/debug/ac --nats-url=${NATS_URL} instance put btrmkr_ml_1 config/instances/btrmkr_ml_1.yaml --host raspineve --mocked
-./target/debug/ac --nats-url=${NATS_URL} instance put tierra_gravity_1 config/instances/tierra_gravity_1.yaml --host raspineve --mocked
+./target/debug/ac --nats-url=${NATS_URL} instance put btrmkr_ml_1 config/instances/btrmkr_ml_1.yaml --host raspineve
+./target/debug/ac --nats-url=${NATS_URL} instance put tierra_gravity_1 config/instances/tierra_gravity_1.yaml --host raspineve
