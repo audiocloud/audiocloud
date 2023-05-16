bcd55a53662146669e02d3e5ac8ae02550194887
Rok Kroflič <rok.kroflic@gmail.com>
2023-05-16 16:33:17 +0200

    Update script.

--- rust/start-domain-server.sh
+++ rust/start-domain-server.sh
@@ -10,4 +10,4 @@ export RUST_LOG=warn
 export NATS_URL=10.1.0.10:4222
 export REST_API_BIND=0.0.0.0:7200
 
-./downloads/domain-server-macos-aarch64-v0.2.2-alpha5 --nats-url=${NATS_URL} --enable-api --enable-instances --rest-api-bind=${REST_API_BIND}
+./target/release/domain-server-macos-aarch64-v0.2.2-alpha5 --nats-url=${NATS_URL} --enable-api --enable-instances --rest-api-bind=${REST_API_BIND}
