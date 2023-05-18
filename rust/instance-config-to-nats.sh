1a03d64e1f772cab4489842868d7344af6efffae
distopik2 <distopik2@Distopiks-Mac-mini.local>
2023-05-18 15:17:37 +0200

    Fix parameter name.

--- rust/instance-config-to-nats.sh
+++ rust/instance-config-to-nats.sh
@@ -7,8 +7,9 @@ export NATS_URL=${NATS_URL:-10.1.0.10:4222}
 
 PATH=$PATH:./target/release
 
-ac instance put ppdu_r1_up_l config/instances/ppdu_r1_up_l.yaml --host "${INSTANCE_HOST}"
-ac instance put btrmkr_ml_1 config/instances/bettermaker/btrmkr_ml_1.yaml --host "${INSTANCE_HOST}"
-ac instance put tierra_gravity_1 config/instances/tierra/tierra_gravity_1.yaml --host "${INSTANCE_HOST}"
-ac instance put distopik_1176_1 config/instances/distopik/distopik_1176_1.yaml --host "${INSTANCE_HOST}"
+ac instance put ppdu_r1_up_l config/instances/ppdu_r1_up_l.yaml --host "${INSTANCE_HOST}" --mocked
+ac instance put btrmkr_ml_1 config/instances/bettermaker/btrmkr_ml_1.yaml --host "${INSTANCE_HOST}" --mocked
+ac instance put tierra_gravity_1 config/instances/tierra/tierra_gravity_1.yaml --host "${INSTANCE_HOST}" --mocked
+ac instance put distopik_1176_1 config/instances/distopik/distopik_1176_1.yaml --host "${INSTANCE_HOST}" --mocked
 ac instance put gyraf_g24_1 config/instances/gyraf/gyraf_g24_1.yaml --host "${INSTANCE_HOST}"
+# ac instance put elysia_nvelope_1 config/instances/elysia/elysia_nvelope_1.yaml --host "${INSTANCE_HOST}"
