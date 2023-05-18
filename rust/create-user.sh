7da2e8b3529d59141257103ce7953df57bbe373e
distopik2 <distopik2@Distopiks-Mac-mini.local>
2023-05-18 14:18:35 +0200

    Update scripts.

--- rust/create-user.sh
+++ rust/create-user.sh
@@ -7,4 +7,4 @@ set -o xtrace
 
 export NATS_URL=${NATS_URL:-10.1.0.10:4222}
 
-./target/release/ac --nats-url="${NATS_URL}" user create 'rok' 'rok.kroflic@gmail.com'
+./target/release/ac --nats-url="${NATS_URL}" user create 'admin' 'rok.kroflic@gmail.com'
--- rust/instance-config-to-nats.sh
+++ rust/instance-config-to-nats.sh
@@ -7,9 +7,9 @@ set -o xtrace
 
 export NATS_URL=${NATS_URL:-10.1.0.10:4222}
 
-./target/release/ac --nats-url=${NATS_URL} instance put ppdu_r1_up_l config/instances/ppdu_r1_up_l.yaml --host raspineve
+./target/release/ac --nats-url=${NATS_URL} instance put ppdu_r1_up_l config/instances/ppdu_r1_up_l.yaml --host raspineve --mocked
 
-./target/release/ac --nats-url=${NATS_URL} instance put btrmkr_ml_1 config/instances/bettermaker/btrmkr_ml_1.yaml --host raspineve
-./target/release/ac --nats-url=${NATS_URL} instance put tierra_gravity_1 config/instances/tierra/tierra_gravity_1.yaml --host raspineve
+./target/release/ac --nats-url=${NATS_URL} instance put btrmkr_ml_1 config/instances/bettermaker/btrmkr_ml_1.yaml --host raspineve --mocked
+./target/release/ac --nats-url=${NATS_URL} instance put tierra_gravity_1 config/instances/tierra/tierra_gravity_1.yaml --host raspineve --mocked
 ./target/release/ac --nats-url=${NATS_URL} instance put distopik_1176_1 config/instances/distopik/distopik_1176_1.yaml --host raspineve
 ./target/release/ac --nats-url=${NATS_URL} instance put gyraf_g24_1 config/instances/gyraf/gyraf_g24_1.yaml --host raspineve
